95113f17b2b405d88b8697713dbc91d37f0c1986
kravetst <taraskravetc28@gmail.com>
2026-02-02 19:11:52 +0200

    Add Pydantic schemas for auth, users and posts

---
+++ app/schemas/post.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import datetime
+from app.schemas.user import UserRead
+
+class PostCreate(BaseModel):
+    title: str
+    content: str
+
+class PostRead(BaseModel):
+    id: int
+    title: str
+    content: str
+    author: UserRead
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
