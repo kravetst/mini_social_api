711010aee9049d9536fcb4858cd23bcc4f2ea239
kravetst <taraskravetc28@gmail.com>
2026-02-03 20:35:55 +0200

    update db session to use core config

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from typing import AsyncGenerator
 
-from app.config import settings
+from app.core.config import settings
 
 
 DATABASE_URL = (
