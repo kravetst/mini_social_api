f1b26105ec5c0b027f8352ed2ad44d86d63b1772
kravetst <taraskravetc28@gmail.com>
2026-02-05 12:40:38 +0200

    Clean up security.py: fix top empty line and format functions

--- app/core/security.py
+++ app/core/security.py
@@ -1,21 +1,22 @@
-
 from datetime import datetime, timedelta
 import jwt
-
 
 from argon2 import PasswordHasher
 from app.core.config import settings
 
 ph = PasswordHasher()
 
+
 def hash_password(password: str) -> str:
     return ph.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     try:
         return ph.verify(hashed_password, plain_password)
     except:
         return False
+
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
